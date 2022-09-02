46b94685000c08c563fadc9dc0da606b6b3f4bc1
MichaelSharoiko <72543232+MichaelSharoiko@users.noreply.github.com>
2022-09-02 10:57:28 -0700

    Create PCA.py

---
+++ PCA.py
@@ -0,0 +1,8 @@
+#first we import our libraries
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+#import our dataset
+from sklearn.datasets import load_wine
